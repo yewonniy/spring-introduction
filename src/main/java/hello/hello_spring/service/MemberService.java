cb1ea7ac09d004224d877abd140db21a304c4ba1
yewonniy <ae6652@naver.com>
2024-06-02 17:52:24 +0900

    section4 스프링 빈과 의존관계

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -2,6 +2,9 @@ package hello.hello_spring.service;
 
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 
